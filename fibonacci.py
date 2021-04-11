3adc59d85ff1fa125c30affc257e0b3f7961e596
Madhumithra916 <82104798+Madhumithra916@users.noreply.github.com>
2021-04-11 18:41:09 +0530

    fibonacci.py
    this program deals with fibonacci series

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+myList=[0,1,2,3,4,5,6,7,8,9,10,11,12]
+n1=0
+n2=1
+print("The fibonacci series are:")
+print(n1)
+print(n2)
+for numbers in myList:
+    n3=n1+n2
+    print(n3)
+    n1=n2
+    n2=n3
+
+    
+    
